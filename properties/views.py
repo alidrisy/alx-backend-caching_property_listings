6c67d37b9ab3fe698b9d5ce7ae400454496d8e5b
alidrisy <a.alidrisy6@gmail.com>
2025-07-12 21:45:45 +0300

    Refactor property_list view to use utility function for fetching properties

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Property
+
+from properties.utils import get_all_properties
 from django.views.decorators.cache import cache_page
 
 
 # Create your views here.
 @cache_page(60 * 15)
 def property_list(request):
-    all_property = Property.objects.all()
+    all_property = get_all_properties()
     return render(request, "products.html", {"products": all_property})
